a5757defaa81ec5377bd48e74470ee1e456d3d4d
HATBE2113 <admin@aarongensetter.ch>
2022-12-03 19:34:12 +0100

    adding register to routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/pages/auth/login/login.component';
+import { RegisterComponent } from './components/pages/auth/register/register.component';
 import { IndexComponent } from './components/pages/index/index.component';
 import { RedirectComponent } from './components/pages/redirect/redirect.component';
 import { DashboardComponent } from './components/pages/userbackend/dashboard/dashboard.component';
@@ -8,6 +9,7 @@ import { DashboardComponent } from './components/pages/userbackend/dashboard/das
 const routes: Routes = [
   {path: '', component: IndexComponent, title: "URL-Shortener"},
   {path: 'login', component: LoginComponent, title: "Login"},
+  {path: 'register', component: RegisterComponent, title: "Register"},
   {path: 'dashboard', component: DashboardComponent, title: "Dashboard"},
   {path: ':id', component: RedirectComponent, title: "Redirect..."},
   {path:'**', component: IndexComponent}
